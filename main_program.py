e7aceb56d1a7470e186321a464025a27cd80d8a6
axelsvenn <mrr.axelsven@gmail.com>
2022-03-05 17:22:19 +0300

    k

--- main_program.py
+++ main_program.py
@@ -7,31 +7,35 @@ app = Flask(__name__)
 def load_photo():
     if request.method == 'GET':
         return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                            crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
-                            <title>Отбор астронавтов</title>
-                          </head>
-                          <body>
-                            <h1 align="center">Загрузка фотографии</h1>
-                            <h2 align="center">для участия в миссии</h2>
-                            <div>
-                            <form method="post" enctype="multipart/form-data">
-                               <div class="form-group">
-                                    <label for="photo">Выберите файл</label><br><br>
-                                    <input type="file" class="form-control-file" id="photo" name="file">
-                                </div><br>
-                                <button type="submit" class="btn btn-primary">Отправить</button>
-                            </form>
-                            </div>
-                          </body>
-                        </html>'''
+                            <html lang="en">
+                              <head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                <link rel="stylesheet"
+                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                crossorigin="anonymous">
+                                <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
+                                <title>Отбор астронавтов</title>
+                              </head>
+                              <body>
+                                <h1 align="center">Загрузка фотографии</h1>
+                                <h2 align="center">для участия в миссии</h2>
+                                <div>
+                                    <form class="login_form" method="post">
+                                        <div class="form-group">
+                                            <label for="photo">Приложите фотографию</label><br>
+                                            <input type="file" class="form-control-file" id="photo" name="file">
+                                        </div>
+                                        <br>
+                                        <img src="{url_for('static', filename='img/photo.png')}"
+                                        alt="здесь должна была быть картинка, но не нашлась">
+                                        <button type="submit" class="btn btn-primary">Отправить</button>
+                                    </form>
+                                </div>
+                              </body>
+                            </html>'''
+
     elif request.method == 'POST':
         with open("static/img/photo.png", 'wb') as fd:
             f = request.files["file"]
