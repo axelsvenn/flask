e4c9f6ded750ef4a82204ef0728a076e48bc0acb
axelsvenn <mrr.axelsven@gmail.com>
2022-03-05 17:04:20 +0300

    k

--- main_program.py
+++ main_program.py
@@ -11,16 +11,17 @@ def load_photo():
                           <head>
                             <meta charset="utf-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                             <link rel="stylesheet"
-                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                             crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
                             <title>Отбор астронавтов</title>
                           </head>
                           <body>
                             <h1 align="center">Загрузка фотографии</h1>
                             <h2 align="center">для участия в миссии</h2>
+                            <div>
                             <form method="post" enctype="multipart/form-data">
                                <div class="form-group">
                                     <label for="photo">Выберите файл</label><br><br>
@@ -28,10 +29,11 @@ def load_photo():
                                 </div><br>
                                 <button type="submit" class="btn btn-primary">Отправить</button>
                             </form>
+                            </div>
                           </body>
                         </html>'''
     elif request.method == 'POST':
-        with open("static/photo.png", 'wb') as fd:
+        with open("static/img/photo.png", 'wb') as fd:
             f = request.files["file"]
             fd.write(f.read())
         return "Форма отправлена"
