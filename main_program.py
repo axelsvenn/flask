b0b27430c4945bde469e69ab6a742d6c114a58b3
axelsvenn <mrr.axelsven@gmail.com>
2022-03-09 19:23:15 +0300

    flask wtf 3 class

--- main_program.py
+++ main_program.py
@@ -1,8 +1,4 @@
-import json
-
-from flask import Flask, render_template, redirect
-
-from loginform import LoginForm
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -17,20 +13,9 @@ def index():
     return render_template('index.html', **param)
 
 
-@app.route('/news')
-def news():
-    with open("news.json", "rt", encoding="utf8") as f:
-        news_list = json.loads(f.read())
-    print(news_list)
-    return render_template('news.html', news=news_list)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
+@app.route("/list_prof/<list>")
+def list_prof(list):
+    return render_template("index.html", list=list)
 
 
 if __name__ == '__main__':
