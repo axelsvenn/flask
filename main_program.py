3ac6b623864e9c5eda7bacb9270827af0242725f
axelsvenn <mrr.axelsven@gmail.com>
2022-03-14 17:07:31 +0300

    sqlalchemy class

--- main_program.py
+++ main_program.py
@@ -1,19 +1,21 @@
 import json
 from data import db_session
 from flask import Flask, render_template, redirect
+
+from data.news import News
+from forms.user import RegisterForm
 from loginform import LoginForm
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    param = {}
-    param['username'] = "Ученик Яндекс.Лицея"
-    param['title'] = 'Домашняя страница'
-    return render_template('index.html', **param)
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 @app.route('/news')
@@ -28,12 +30,42 @@ def news():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/success')
+        return redirect('/')
     return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 def main():
     db_session.global_init("db/blogs.db")
+    # db_sess = db_session.create_session()
+    # news = News(title="Вторая новость", content="Привет блог! ДА",
+    #             user_id=1, is_private=True)
+    # db_sess.add(news)
+    # db_sess.commit()
     app.run()
 
 
